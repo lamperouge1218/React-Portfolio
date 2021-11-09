de696e886fcec298108512cb02ee1c639c920a70
lamperouge <philbohn1791@gmail.com>
2021-11-09 11:24:07 -0500

    changed navbar folder to be called header for consistency

--- src/components/PorftolioContainer.js
+++ src/components/PorftolioContainer.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Header from "./navbar/Header";
+import Header from "./header/Header";
 import About from "./pages/About";
 import Portfolio from "./pages/Porftolio";
 import Contact from "./pages/Contact";
