922cca226359c2eeded991c6a859100d0c680555
lamperouge <philbohn1791@gmail.com>
2021-11-08 21:16:48 -0500

    torching and restarting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <PortfolioContainer />;
+function App() {
+  return <></>;
+}
 
 export default App;
--- src/components/pages/Portfolio.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Porftolio() {
-    return (
-      <div>
-        <h1>Porftolio Page</h1>
-      </div>
-    );
-  }
