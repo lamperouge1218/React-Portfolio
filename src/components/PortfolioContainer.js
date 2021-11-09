328933190c23dac373668ac7469ba487eae6d981
lamperouge <philbohn1791@gmail.com>
2021-11-08 20:24:03 -0500

    further set up file structure, starting to fill out PortfolioContainer.js

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
