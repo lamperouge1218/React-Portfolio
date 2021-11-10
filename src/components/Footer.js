ccbaaa9e066e75542955ab1cd9cd1cd2ddbe77df
lamperouge <philbohn1791@gmail.com>
2021-11-10 14:49:17 -0500

    got footer seemingly functioning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Footer() {
   return (
     <footer className="py-3 mt-4 bg-dark bg-gradient footer-style">
-      <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+      <ul className="nav justify-content-center pb-3 mb-3">
         <li className="nav-item">
           <a
             href="mailto:philbohn1791@gmail.com"
@@ -34,7 +34,6 @@ export default function Footer() {
           </a>
         </li>
       </ul>
-      <p className="text-center text-light">© 2021 Phil Bohn</p>
     </footer>
   );
 }
